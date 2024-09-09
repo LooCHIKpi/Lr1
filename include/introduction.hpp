4fdf33d94c4e15a27f6a8d8539e0d7988b95b50d
Данила Александрович Боргояков <8221045@vc.miet.ru>
2024-09-09 17:27:20 +0300

    Commentaries

---
+++ include/introduction.hpp
@@ -0,0 +1,35 @@
+/* Introduction class.
+ * @file
+ * @date 2021-09-04
+ * @author Anonymous
+ */
+
+#ifndef __INTRODUCTION_HPP__
+#define __INTRODUCTION_HPP__
+
+#include <string>
+
+//{
+class introduction
+{
+public:
+    std::string_view name() const
+    {
+        return Danil
+    }
+
+    std::string_view surename() const
+    {
+        return Borgoyakov
+    }
+
+    std::string fullname() const
+    {
+        return Danil_Borgoyakov
+    }
+
+private:
+};
+//}
+
+#endif // __INTRODUCTION_HPP__
